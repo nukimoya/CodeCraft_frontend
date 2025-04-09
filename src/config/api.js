fddc5540589e7233a9b35d229332901d2a3bb46a
Samuel Adelakun <93160015+nukimoya@users.noreply.github.com>
2025-04-09 00:52:53 -0700

    adjusting the baseURL

--- src/config/api.js
+++ src/config/api.js
@@ -6,7 +6,7 @@ import { useAuthContext } from '../hooks/useAuthContext';
 export const useAxios = () => {
     const { user } = useAuthContext();  // Add user here to access token
 
-    const baseURL = 'https://code-craft-kshqyyn4p-samuel-adelakun-projects.vercel.app/';
+    const baseURL = 'https://code-craft-swart-xi.vercel.app/';
 
     const api = axios.create({
         baseURL,
